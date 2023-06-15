07ec09e474c1c6e6e3c9bd29fc1dfc4c4427088a
Æscanor <113015001+Aescanor@users.noreply.github.com>
2023-06-15 22:35:55 +0200

    ended filter - todo css

--- Filter.js
+++ Filter.js
@@ -1,15 +1,21 @@
 // Filter.js
-import React, { useState } from 'react';
-import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, TextInput, Button, StyleSheet, Switch } from 'react-native';
 
 const Filter = ({ onFilter }) => {
   const [minPrice, setMinPrice] = useState('0');
   const [maxPrice, setMaxPrice] = useState('0');
+  const [transmission, setTransmission] = useState(false);
+  const [climatisation, setClimatisation] = useState(false);
+
+  useEffect(() => {
+    handleFilter();
+  }, [minPrice, maxPrice, transmission, climatisation]);
 
   const handleFilter = () => {
     const min = parseInt(minPrice);
     const max = parseInt(maxPrice);
-    onFilter(min, max);
+    onFilter(min, max, transmission, climatisation);
   };
 
   return (
@@ -28,7 +34,21 @@ const Filter = ({ onFilter }) => {
         onChangeText={(number) => setMaxPrice(number)}
         keyboardType="numeric"
       />
-      <Button title="Filtrer" onPress={handleFilter} />
+      <View style={styles.switchContainer}>
+        <Text>Boîte automatqiue:</Text>
+        <Switch
+          value={transmission}
+          onValueChange={(value) => setTransmission(value)}
+        />
+      </View>
+      <View style={styles.switchContainer}>
+        <Text>Climatisation :</Text>
+        <Switch
+          value={climatisation}
+          onValueChange={(value) => setClimatisation(value)}
+        />
+      </View>
+      {/* <Button title="Filtrer" onPress={handleFilter} /> */}
     </View>
   );
 };
@@ -44,6 +64,11 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     marginBottom: 10,
     paddingHorizontal: 10,
+  },
+  switchContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 10,
   },
 });
 
